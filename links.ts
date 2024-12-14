5e453f618377271ee0901b36e27f505064070309
birkaevs <birkaevs@outlook.com>
2024-12-14 21:32:49 +0100

    Create links.ts

---
+++ links.ts
@@ -0,0 +1,26 @@
+// links.ts
+export const links = [
+  {
+    link: "https://site.com",
+    geo: "US",
+    mobile: true,
+    desktop: false,
+  },
+  {
+    link: "https://website.com",
+    geo: "US",
+    mobile: true,
+    desktop: true,
+  },
+  {
+    link: "https://example.com",
+    geo: "CA",
+    mobile: true,
+    desktop: true,
+  },
+];
+
+export const defaultLinks = [
+  "https://fallback1.com",
+  "https://fallback2.com",
+];
